461337fbbc6a8aaeda08f6371353941c87b75adc
Mauro Moreno <mmauromoreno1@gmail.com>
2023-12-04 21:35:33 -0300

    Implemented column with category

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    {_id: '1', name: 'Unity', category: 'Games'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // this.courses = [];
+  }
+
+  ngOnInit(): void {
+  }
 }
